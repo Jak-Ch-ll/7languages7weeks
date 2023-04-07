1079ca346af91df689b506471d57444d4c7fc49c
J.c <Jak-Ch-ll@mailbox.org>
2023-04-07 19:42:45 +0200

    finish Prolog day1

---
+++ prolog/day1/movies.pl
@@ -0,0 +1,7 @@
+directs(nolan, inception).
+directs(nolan, interstellar).
+directs(russos, endgame).
+directs(russos, winter_soldier).
+directs(snyder, justice_league).
+directs(favreau, iron_man).
+directs(favreau, lion_king).
